8f67e1bbed00ddd6ee85061efc5f97b9a09d7108
Born2Ride <ccirelli2@gmail.com>
2018-04-06 09:16:37 -0400

    HW2 Updates

--- HW2/HW2.py
+++ HW2/HW2.py
@@ -14,14 +14,17 @@ conf = SparkConf().setMaster('local').setAppName('HW2.py')
 sc = SparkContext(conf = conf)
 
 
-# Import Python Functions
+# IMPORT PYTHON LIBRARIES 
 import string
 
-# Create RDD
+
+# CREATE RDD
 '''Import CSV file as a Text RDD'''
+
 Ratings_data = sc.textFile('/home/ccirelli2/Desktop/Scalable_Analytics/HW2/Amazon_Comments.csv')
 
-# Transformation Function
+
+# CREATE PAIR RDD
 '''Documentation:
 	1.) Create a top-level function called 'Transform' to 
 		a.) Limit the return value to index 5 (text of review) and index 6 (rating)
@@ -35,16 +38,33 @@ def CreateKeyValuePair(Lists):
 	punct = string.punctuation
 	return Lists[6], len(list(filter(lambda x: x not in punct, Lists[5].split(' ')))) 
 
-# Transform RDD
+
+# TRANSFORM RDD
 '''Documentation
 	1.) Execute a series of RDD transformation functions to
 		a.) Split text on each line
 		b.) Split each line on '^'
 		c.) Map our top-level function to this RDD
 '''
+
 Split_data = Ratings_data.flatMap(lambda x: x.split('\n')).map(lambda x: x.split('^')).map(CreateKeyValuePair)
 
-print('#######', Split_data.take(2))
+
+
+# GROUP RDD BY KEY
+Group_by_key = Split_data.groupByKey()
+
+
+
+# GET AVERAGE LENGTH REVIEW
+
+def Get_average(KeyValue):
+	return KeyValue[0], round(sum(list(KeyValue[1])) / len(list(KeyValue[1])),2)
+
+Average_length = Group_by_key.map(Get_average)
+
+
+print('#######', Average_length.collect())
 
 
 
